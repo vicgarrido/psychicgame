09d042d52d41f639c11a5cbcb431033036659f13
Victor Garrido <victorgarrido@victors-mbp-2.attlocal.net>
2018-07-11 21:40:40 -0400

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// letters
+var letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',]
+
+// Variables
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessLeft = 9;
+var guessed = [];
+var letterguess = null;
+
+var headsCount = 0;
+var tailsCount = 0;
+var wins = 0;
+var losses = 0;
